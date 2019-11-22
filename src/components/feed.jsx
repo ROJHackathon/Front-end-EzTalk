decf51652c281f719671f00684a4b876416df0da
Yangtao Ge <geyangtao@yahoo.com>
2019-11-22 23:37:43 +0000

    Refectory feed  in Card list

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -32,17 +32,17 @@ class Feed extends React.Component {
                 {
                     title: 'Yellow Submarine',
                     author: 'Beatles',
-                    cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-1.jpg',
+                    cover: 'https://cdn.framework7.io/placeholder/nature-1000x600-1.jpg',
                 },
                 {
                     title: 'Don\'t Stop Me Now',
                     author: 'Queen',
-                    cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-2.jpg',
+                    cover: 'https://cdn.framework7.io/placeholder/nature-1000x600-2.jpg',
                 },
                 {
                     title: 'Billie Jean',
                     author: 'Michael Jackson',
-                    cover: 'https://cdn.framework7.io/placeholder/abstract-88x88-3.jpg',
+                    cover: 'https://cdn.framework7.io/placeholder/nature-1000x600-3.jpg',
                 },
             ],
             songs: ['Yellow Submarine', 'Don\'t Stop Me Now', 'Billie Jean', 'Californication'],
@@ -55,28 +55,29 @@ class Feed extends React.Component {
                 <Navbar title="Pull To Refresh"></Navbar>
                 <List mediaList>
                     {this.state.items.map((item, index) => (
-                        <ListItem
-                            key={index}
-                            title={item.title}
-                            subtitle={item.author}
-                        >
-                            <img slot="media" src={item.cover} width="44" />
-                        </ListItem>
-                        // <Card className="demo-card-header-pic">
-                        //     <CardHeader
-                        //         className="no-border"
-                        //         valign="bottom"
-                        //         style={ {backgroundImage: "url(" + {item} + ")"} }
-                        //     >{item.title}</CardHeader>
-                        //     <CardContent>
-                        //         <p className="date">Posted on January 21, 2015</p>
-                        //         <p>{item.author}</p>
-                        //     </CardContent>
-                        //     <CardFooter>
-                        //         <Link>Like</Link>
-                        //         <Link>Read more</Link>
-                        //     </CardFooter>
-                        // </Card>
+                        // <ListItem
+                        //     key={index}
+                        //     title={item.title}
+                        //     subtitle={item.author}
+                        // >
+                        //     <img slot="media" src={item.cover} width="44"/>
+                        // </ListItem>
+                        <Card className="feed-card"
+                                key = {index}>
+                            <CardHeader className = "card-header"
+                                className="no-border"
+                                valign="bottom"
+                                style= {{backgroundImage: "url(" + item.cover + ")"} }
+                            >{item.title}</CardHeader>
+                            <CardContent>
+                                <p className="date">Posted on January 21, 2015</p>
+                                <p>{item.author}</p>
+                            </CardContent>
+                            <CardFooter>
+                                <Link>Like</Link>
+                                <Link>Read more</Link>
+                            </CardFooter>
+                        </Card>
                     ))}
                 </List>
             </Page>
