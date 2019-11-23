f53ea1689b513f2d2340befcaea6de0249d84cfe
Yangtao Ge <geyangtao@yahoo.com>
2019-11-23 12:59:42 +0000

    Enable request feed

--- src/components/feedTest.jsx
+++ src/components/feedTest.jsx
@@ -34,29 +34,50 @@ class FeedTest extends React.Component {
         }
     }
 
-
+    //first feed when init the app 
     componentDidMount() {
-        axios.get('http://108.61.221.218:58447/api-fake/user/10/request-feed').then(res => {
+        let url = 'http://108.61.221.218:58447/api-fake/user/' + 10 + '/request-feed'; // 10 is the user id
+        axios.get(url).then(res => {
             console.log(res);
-            this.setState({materials: res.data});
+            this.setState({ materials: res.data });
         });
     }
 
-    render(){
-        return(
-            <List mediaList>
-                {this.state.materials.map((material, index) => (
-                    <ListItem
-                        key = {index}
-                        title = {material.title}
-                        subtitle = {material.description}
-                    >
-                        <img slot="media" src ="https://cdn.framework7.io/placeholder/nature-1000x600-3.jpg" width="44"/>
-                    </ListItem>
-                ))}
-            </List>
+    render() {
+        return (
+            <Page ptr onPtrRefresh={this.loadMore.bind(this)}>
+                <List mediaList>
+                    {this.state.materials.map((material, index) => (
+                        <ListItem
+                            key={index}
+                            title={material.title}
+                            subtitle={material.description}
+                        >
+                            <img slot="media" src="https://cdn.framework7.io/placeholder/nature-1000x600-3.jpg" width="44" />
+                        </ListItem>
+                    ))}
+                </List>
+            </Page>
         )
     };
+
+    loadMore(done) {
+        const self = this;
+        setTimeout(() => {
+            const { materials } = self.state;
+            let url = 'http://108.61.221.218:58447/api-fake/user/' + 10 + '/request-feed';
+            axios.get(url).then(res => {
+                //console.log(res);
+                let newList = materials.concat(res.data);
+                self.setState({materials: newList});
+                console.log(newList);
+            });
+
+            done();
+        }, 1000)
+    }
+
 }
+
 
 export default FeedTest;
