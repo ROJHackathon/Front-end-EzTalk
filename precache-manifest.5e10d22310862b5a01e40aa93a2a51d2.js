aa4661f86354ce5e7c3e7852d116d3e2079756dd
Yangtao Ge <geyangtao@yahoo.com>
2020-01-09 23:43:45 +0000

    Updates

---
+++ precache-manifest.5e10d22310862b5a01e40aa93a2a51d2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22543d40e85fd68417266770aabc393",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d59bf8eb8cb2566314f",
+    "url": "css/app.css"
+  },
+  {
+    "revision": "9ee96e3a07dec6235d3f1a86787a92b7",
+    "url": "fonts/Framework7Icons-Regular.eot"
+  },
+  {
+    "revision": "3b973a05c0e1544f7ee8fa23c2a9315a",
+    "url": "fonts/Framework7Icons-Regular.ttf"
+  },
+  {
+    "revision": "535bcf7dd3feb41ec20507c3c5b81efb",
+    "url": "fonts/Framework7Icons-Regular.woff"
+  },
+  {
+    "revision": "1dfb73e22b9119f547fb880568f56ea2",
+    "url": "fonts/Framework7Icons-Regular.woff2"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "fonts/MaterialIcons-Regular.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "fonts/MaterialIcons-Regular.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "8c4a5d8c7432b6bf1c5a80135816d0b8",
+    "url": "images/loginBackground.jpg"
+  },
+  {
+    "revision": "8d59bf8eb8cb2566314f",
+    "url": "js/app.js"
+  },
+  {
+    "revision": "5b64c241347aae87355cc33b236e88bb",
+    "url": "js/app.js.LICENSE"
+  },
+  {
+    "revision": "8365c368e01a2a15972c27959a112a08",
+    "url": "manifest.json"
+  }
+]);
