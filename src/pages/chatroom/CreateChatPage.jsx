8144fa1712d2db8ffd9acb70deb999b55936b785
Yangtao Ge <geyangtao@yahoo.com>
2020-01-10 19:48:57 +0000

    add create button

--- src/pages/chatroom/CreateChatPage.jsx
+++ src/pages/chatroom/CreateChatPage.jsx
@@ -24,7 +24,7 @@ import {
 } from 'framework7-react';
 import axios from 'axios';
 
-export default class Material extends React.Component {
+export default class CreateChatPage extends React.Component {
 
     constructor(props) {
         super(props);
@@ -40,68 +40,50 @@ export default class Material extends React.Component {
             <Page>
                 <Navbar title="Create Your Chat Room" backLink="Back" />
 
-                <List noHairlinesMd inset>
-                    <ListInput
-                        label="Name"
-                        floatingLabel
-                        type="text"
-                        placeholder="Your name"
-                        clearButton
-                    >
-                        <Icon icon="demo-list-icon" slot="media"/>
-                    </ListInput>
+                <BlockTitle large>Chat Room Info</BlockTitle>
+                <Block>
+                    <p> This page will only a create private chat room, which cannot be accessed without permission.
+                        Public chat room cannot be created by the administrator.
+                    </p>
+                </Block>
 
-                    <ListInput
-                        label="Password"
-                        floatingLabel
-                        type="password"
-                        placeholder="Your password"
-                        clearButton
-                    >
-                        <Icon icon="demo-list-icon" slot="media"/>
-                    </ListInput>
+                    <List noHairlinesMd inset>
+                        <ListInput
+                            label="Name"
+                            floatingLabel
+                            type="text"
+                            placeholder="Chat Room Name"
+                            clearButton
+                            info="Make a nice name for your chat room"
+                            required
+                            validate
+                        >
+                        </ListInput>
 
-                    <ListInput
-                        label="E-mail"
-                        floatingLabel
-                        type="email"
-                        placeholder="Your e-mail"
-                        clearButton
-                    >
-                        <Icon icon="demo-list-icon" slot="media"/>
-                    </ListInput>
+                        <ListInput
+                            label="Language"
+                            type="select"
+                            defaultValue="English"
+                            placeholder="Please choose..."
+                        >
+                            <option value="Arabic">Arabic</option>
+                            <option value="Chinese">Chinese</option>
+                            <option value="English">English</option>
+                            <option value="French">French</option>
+                            <option value="Spanish">Spanish</option>
+                        </ListInput>
 
-                    <ListInput
-                        label="URL"
-                        floatingLabel
-                        type="url"
-                        placeholder="URL"
-                        clearButton
-                    >
-                        <Icon icon="demo-list-icon" slot="media"/>
-                    </ListInput>
+                        <ListInput
+                            label="Description"
+                            floatingLabel
+                            type="textarea"
+                            resizable
+                            placeholder="Bio"
+                        >
+                        </ListInput>
+                    </List>
 
-                    <ListInput
-                        label="Phone"
-                        floatingLabel
-                        type="tel"
-                        placeholder="Your phone number"
-                        clearButton
-                    >
-                        <Icon icon="demo-list-icon" slot="media"/>
-                    </ListInput>
-
-                    <ListInput
-                        label="Resizable"
-                        floatingLabel
-                        type="textarea"
-                        resizable
-                        placeholder="Bio"
-                    >
-                        <Icon icon="demo-list-icon" slot="media"/>
-                    </ListInput>
-                </List>
-
+                <Button raised fill round className="create-chat-btn">Create</Button>
 
             </Page>
         );
