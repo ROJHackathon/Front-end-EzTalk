26a4471c57f2453cc3a2c9ffd51642a300aa62a7
Yangtao Ge <geyangtao@yahoo.com>
2020-01-10 17:44:01 +0000

    add search page in chatroom

--- src/pages/chatroom/chatroom.jsx
+++ src/pages/chatroom/chatroom.jsx
@@ -29,13 +29,17 @@ export default class extends React.Component {
         super();
         this.state = {
             activeTag: "1",
+            onSearch: false,
         };
 
         this.handleActiveTag = this.handleActiveTag.bind(this)
+        this.handleEnableSearch = this.handleEnableSearch.bind(this)
+        this.handleDisableSearch = this.handleDisableSearch.bind(this)
     }
 
     render() {
-        console.log(this.state.activeTag);
+        //console.log(this.state.activeTag);
+        //console.log(this.state.onSearch)
         return (
             <Page name="chatroom">
                 <Navbar sliding={false} large>
@@ -51,15 +55,37 @@ export default class extends React.Component {
                             searchIn=".item-title"   // what is the query
                             placeholder="Search Chatroom"
                             clearButton={true}
+                            onSearchbarEnable={this.handleEnableSearch}
+                            onSearchbarDisable={this.handleDisableSearch}
                         />
                     </Subnavbar>
                 </Navbar>
-                <RoomsBase handleActiveTag={this.handleActiveTag}/>
+
+                {!this.state.onSearch ?  <RoomsBase handleActiveTag={this.handleActiveTag}/> : <div/>}
+
+
+
                 <Fab position="right-bottom" slot="fixed" color="blue" href="/create-chat/">
                     <Icon f7="plus"/>
                 </Fab>
 
             </Page>
+        )
+    }
+
+    handleEnableSearch() {
+        this.setState(
+            {
+                onSearch: true
+            }
+        )
+    }
+
+    handleDisableSearch() {
+        this.setState(
+            {
+                onSearch: false
+            }
         )
     }
 
