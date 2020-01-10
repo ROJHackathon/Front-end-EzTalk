5c4a7e81010a193bd7fb2c6835e0e6f9b51b2af1
Yangtao Ge <geyangtao@yahoo.com>
2020-01-10 11:15:34 +0000

    debug refresh problem

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import {
-  Page,
-  Navbar,
-  NavLeft,
-  NavTitle,
-  NavTitleLarge,
-  NavRight,
-  Link,
-  Toolbar,
-  Block,
-  BlockTitle,
-  List,
-  ListItem,
-  Row,
-  Col,
-  Button,
-  Subnavbar,
-  Searchbar
+    Page,
+    Navbar,
+    NavLeft,
+    NavTitle,
+    NavTitleLarge,
+    NavRight,
+    Link,
+    Toolbar,
+    Block,
+    BlockTitle,
+    List,
+    ListItem,
+    Row,
+    Col,
+    Button,
+    Subnavbar,
+    Searchbar
 } from 'framework7-react';
 
 //self-defined component
@@ -28,132 +28,141 @@ import TokenContext from '../../components/tokenContext.jsx'
 import axios from "axios";
 
 export default class extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      onSearch: false,
-      page:1,
-      materials:[]
-    }
-    this.enableSearch = this.enableSearch.bind(this)
-    this.disableSearch = this.disableSearch.bind(this)
-  }
+    constructor() {
+        super()
+        this.state = {
+            onSearch: false,
+            page: 1,
+            materials: [],
 
-  static contextType = TokenContext;
-
-  componentDidMount() {
-    if(this.context != 0){
-      const self = this;
-      const { materials, page } = self.state;
-      if(this.props !== prevProps){
-        let url = 'https://ez-talk-api-provider.azurewebsites.net/api-fake/request-feed?page=' + this.state.page + '&token=' + this.context;
-        axios.get(url).then(res => {
-          //console.log(res);
-          let prevList = materials;
-          let newList = res.data;
-          newList = newList.concat(prevList);
-          self.setState({
-            materials: newList,
-            page: page+1,
-          });
-          //console.log(this.state.page);
-        });
-      }
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    const self = this;
-    const { materials, page } = self.state;
-    if(this.props !== prevProps){
-      let url = 'https://ez-talk-api-provider.azurewebsites.net/api-fake/request-feed?page=' + this.state.page + '&token=' + this.context;
-      axios.get(url).then(res => {
-        //console.log(res);
-        let prevList = materials;
-        let newList = res.data;
-        newList = newList.concat(prevList);
-        self.setState({
-          materials: newList,
-          page: page+1,
-        });
-        //console.log(this.state.page);
-      });
-    }
-  }
-
-
-  enableSearch() {
-    this.setState(
-      {
-        onSearch:true
-      }
-    )
-  }
-
-  disableSearch() {
-    this.setState(
-      {
-        onSearch:false
-      }
-    )
-  }
-
-  render() {
-    return (
-      <Page name="home" ptr onPtrRefresh={this.loadMore.bind(this)}>
-        {/* Top Navbar */}
-        <Navbar sliding={false} large>
-          <NavTitle sliding>Home</NavTitle>
-          <NavRight>
-            <Link iconIos="f7:person_crop_circle" iconAurora="f7:person_crop_circle" iconMd="material:menu" panelOpen="right" />
-          </NavRight>
-          <NavTitleLarge>Home</NavTitleLarge>
-          <Subnavbar inner={false}>
-            <Searchbar
-              searchContainer=".search-list"  // where does the search happen
-              searchIn=".item-title"   // what is the query
-              placeholder="Search Materials"
-              clearButton={true}
-              onSearchbarEnable={this.enableSearch}
-              onSearchbarDisable={this.disableSearch}
-            />
-          </Subnavbar>
-        </Navbar>
-
-        {!this.state.onSearch ? <Feed materials={this.state.materials}/> : <Search/>}
-
-
-      </Page>
-    )
-  };
-
-
-  loadMore(done) {
-    const self = this;
-
-    // if in feed mode
-    if (!this.state.onSearch){
-      setTimeout(() => {
-        const { materials, page } = self.state;
-        let url = 'https://ez-talk-api-provider.azurewebsites.net/api-fake/request-feed?page=' + this.state.page + '&token=' + this.context;
-        axios.get(url).then(res => {
-          //console.log(res);
-          let prevList = materials;
-          let newList = res.data;
-          newList = newList.concat(prevList);
-          self.setState({
-            materials: newList,
-            page: page+1,
-          });
-          //console.log(this.state.page);
-        });
-        done();
-      }, 1000)
-    }
-    //if in search mode
-    else{
-
+            init: true,
+        }
+        this.enableSearch = this.enableSearch.bind(this)
+        this.disableSearch = this.disableSearch.bind(this)
     }
 
-  }
+    static contextType = TokenContext;
+
+    componentDidMount() {
+        //console.log("mount")
+        if (this.context != 0) {
+            const self = this;
+            const {materials, page} = self.state;
+            let url = 'https://ez-talk-api-provider.azurewebsites.net/api-fake/request-feed?page=' + this.state.page + '&token=' + this.context;
+            axios.get(url).then(res => {
+                //console.log(res);
+                let prevList = materials;
+                let newList = res.data;
+                newList = newList.concat(prevList);
+                self.setState({
+                    materials: newList,
+                    page: page + 1,
+                });
+                //console.log(this.state.page);
+            });
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const self = this;
+        const {materials, page} = self.state;
+        if (this.props !== prevProps && this.state.init ) {
+            // set not init
+            self.setState({
+                init: false,
+            });
+
+            // get new context
+            let url = 'https://ez-talk-api-provider.azurewebsites.net/api-fake/request-feed?page=' + this.state.page + '&token=' + this.context;
+            axios.get(url).then(res => {
+                //console.log(res);
+                let prevList = materials;
+                let newList = res.data;
+                newList = newList.concat(prevList);
+
+                self.setState({
+                    materials: newList,
+                    page: page + 1,
+                });
+                //console.log(this.state.page);
+            });
+        }
+    }
+
+
+    enableSearch() {
+        this.setState(
+            {
+                onSearch: true
+            }
+        )
+    }
+
+    disableSearch() {
+        this.setState(
+            {
+                onSearch: false
+            }
+        )
+    }
+
+    render() {
+        return (
+            <Page name="home" ptr onPtrRefresh={this.loadMore.bind(this)}>
+                {/* Top Navbar */}
+                <Navbar sliding={false} large>
+                    <NavTitle sliding>Home</NavTitle>
+                    <NavRight>
+                        <Link iconIos="f7:person_crop_circle" iconAurora="f7:person_crop_circle" iconMd="material:menu"
+                              panelOpen="right"/>
+                    </NavRight>
+                    <NavTitleLarge>Home</NavTitleLarge>
+                    <Subnavbar inner={false}>
+                        <Searchbar
+                            searchContainer=".search-list"  // where does the search happen
+                            searchIn=".item-title"   // what is the query
+                            placeholder="Search Materials"
+                            clearButton={true}
+                            onSearchbarEnable={this.enableSearch}
+                            onSearchbarDisable={this.disableSearch}
+                        />
+                    </Subnavbar>
+                </Navbar>
+
+                {!this.state.onSearch ? <Feed materials={this.state.materials}/> : <Search/>}
+
+
+            </Page>
+        )
+    };
+
+
+    loadMore(done) {
+        const self = this;
+
+        // if in feed mode
+        if (!this.state.onSearch) {
+            setTimeout(() => {
+                const {materials, page} = self.state;
+                let url = 'https://ez-talk-api-provider.azurewebsites.net/api-fake/request-feed?page=' + this.state.page + '&token=' + this.context;
+                axios.get(url).then(res => {
+                    //console.log(res);
+                    let prevList = materials;
+                    let newList = res.data;
+                    newList = newList.concat(prevList);
+                    self.setState({
+                        materials: newList,
+                        page: page + 1,
+                    });
+                    //console.log(this.state.page);
+                });
+                done();
+            }, 1000)
+        }
+        //if in search mode
+        else {
+
+        }
+
+    }
 }
--- src/pages/home/material.jsx
+++ src/pages/home/material.jsx
@@ -37,31 +37,6 @@ export default class Material extends React.Component {
                 <Navbar title={this.state.material.title} backLink="Back" />
                 <MarterialCard id={this.$f7route.params.id}></MarterialCard>
                 <RatingArea></RatingArea>
-                {/* <Block strong>
-                    <ul>
-                        <li><b>Url:</b> {this.$f7route.url}</li>
-                        <li><b>Path:</b> {this.$f7route.path}</li>
-                        <li><b>Hash:</b> {this.$f7route.hash}</li>
-                        <li><b>Params:</b>
-                            <ul>
-                                {Object.keys(this.$f7route.params).map(key => (
-                                    <li key={key}><b>{key}:</b> {this.$f7route.params[key]}</li>
-                                ))}
-                            </ul>
-                        </li>
-                        <li><b>Query:</b>
-                            <ul>
-                                {Object.keys(this.$f7route.query).map(key => (
-                                    <li key={key}><b>{key}:</b> {this.$f7route.query[key]}</li>
-                                ))}
-                            </ul>
-                        </li>
-                        <li><b>Route:</b> {this.$f7route.route.path}</li>
-                    </ul>
-                </Block>
-                <Block strong>
-                    <Link onClick={() => this.$f7router.back()}>Go back via Router API</Link>
-                </Block> */}
 
                 <MaterialComment id={this.$f7route.params.id}></MaterialComment>
             </Page>
